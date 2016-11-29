579a5a61679b6343a84895a268c3264a23f03898
Coding Chefs <codingchefs@gmail.com>
2016-11-29 18:38:41 -0500

    updated for Assignment

---
+++ javascript/11_22_2016/veena/veena.js
@@ -0,0 +1,32 @@
+// 1. Number
+
+var a = 87;
+var b = 98;
+
+
+// 2. String
+
+var firstName = "veena";
+var lastName = "tatikonda";
+
+// 3. Boolean
+var twoIsAEvenNumber = true;
+var threeIsaEvenNumber = false;
+
+// 4. null
+
+var randomVariable = null;
+
+// 5. undefined
+
+var undefinedVar;
+
+console.log(a);
+console.log(b);
+console.log(firstName);
+console.log(lastName);
+console.log(twoIsAEvenNumber);
+console.log(threeIsaEvenNumber);
+console.log(randomVariable);
+console.log(undefinedVar);
+
